e4e5b55a28b6b31b28550fe9dd14680898369975
Aleks <malykhinal@mail.ru>
2026-04-09 14:34:50 +0500

    fixes

--- backend/config.py
+++ backend/config.py
@@ -4,14 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    # SeaTable настройки
     SEATABLE_BASE_URL = os.getenv('SEATABLE_URL', 'https://ditable.yanao.ru')
     SEATABLE_API_TOKEN = os.getenv('SEATABLE_TOKEN', '0d587e5bfac8f558d0a908d871c50db1fee0d047')
     SEATABLE_BASE_UUID = os.getenv('SEATABLE_BASE_UUID', '572d2646-73b9-4a2f-bd19-03ca42b4ceef')
     SEATABLE_TABLE_NAME = os.getenv('SEATABLE_TABLE_NAME', 'Справочник телефонов')
-    
-    # CORS настройки
     CORS_ORIGINS = os.getenv('CORS_ORIGINS', '*').split(',')
-    
-    # Часовой пояс
-    TZ = os.getenv('TZ', 'Asia/Yekaterinburg')
